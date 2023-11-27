03a0f22453d0c447cb423d4babd3fdb7f5c63fe3
bolat <bolatmamyr123@gmail.com>
2023-11-27 15:20:33 +0600

    UI of sign in page done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-ktx:1.8.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/authapplication/SplashScreenActivity.kt
+++ app/src/main/java/com/example/authapplication/SplashScreenActivity.kt
@@ -1,6 +1,7 @@
 package com.example.authapplication
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -16,10 +17,23 @@ class SplashScreenActivity : AppCompatActivity() {
             {
                 Intent(this, MainActivity::class.java).also {
                     startActivity(it)
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                        overrideActivityTransition(
+                            OVERRIDE_TRANSITION_CLOSE,
+                            com.google.android.material.R.anim.abc_fade_in,
+                            com.google.android.material.R.anim.abc_fade_out
+                        )
+                    } else {
+                        @Suppress("DEPRECATION")
+                        overridePendingTransition(
+                            com.google.android.material.R.anim.abc_fade_in,
+                            com.google.android.material.R.anim.abc_fade_out
+                        )
+                    }
                     finish()
                 }
             },
-            3000
+            2000
         )
     }
 }
